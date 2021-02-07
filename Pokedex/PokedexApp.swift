904f0ecb08df226a5a2b62972bf44e67803c9a56
Ethan Borrowman <ethan_borrowman@hotmail.com>
2021-02-06 22:38:31 -0500

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Ethan Borrowman on 2/6/21.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
