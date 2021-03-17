12e44394d8aa916c03cbd9b5aa777a139b06dc7e
Ethan Borrowman <ethan_borrowman@hotmail.com>
2021-03-16 22:01:14 -0400

    Minor UI updates

--- Pokedex/View/PokemonDetails/PokemonStatsView.swift
+++ Pokedex/View/PokemonDetails/PokemonStatsView.swift
@@ -9,25 +9,37 @@ import SwiftUI
 
 struct PokemonStatsView: View {
     private let gridItems = [GridItem(.flexible()), GridItem(.flexible())]
+    private let statFrameWidth = UIScreen.screenWidth * 0.8
     
     var body: some View {
         VStack {
             Text("Stats")
-            HStack {
+                .fontWeight(.medium)
+                .padding(.bottom, 5)
+                .font(.system(size: 20))
+            HStack(spacing: 10) {
                 Text("Height: 68")
+                    .fontWeight(.regular)
                 Text("Weight: 195")
             }
             
             ForEach(0..<6) { count in
-                HStack {
+                HStack (alignment: .center, spacing: 15){
                     Text("Stat \(count)")
                         .foregroundColor(.black)
-                    ProgressView(value: Float16(count)/6)
-                        .padding([.trailing], 10)
+                    ZStack (alignment: .leading) {
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(Color.blue.opacity(0.25))
+                            .frame(width: statFrameWidth * 0.8, height: 25, alignment: .leading)
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(Color.blue)
+                            .frame(width: (statFrameWidth * 0.8)/2, height: 25, alignment: .leading)
+                    }
+                    .background((Color.clear))
                 }
             }
         }
-        .frame(width: UIScreen.main.bounds.width * 0.75, height: 200, alignment: .leading)
+        .frame(width: statFrameWidth, alignment: .center)
     }
 }
 
